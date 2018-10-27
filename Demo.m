158218dc53aeeee555044cb7c9ae132db03f58a7
Yiwen Mei <32801294+YiwenMei@users.noreply.github.com>
2018-10-27 18:02:25 -0400

    Add files via upload

--- Demo.m
+++ Demo.m
@@ -1,38 +1,37 @@
-path='C:\Users\Mei Yiwen\Desktop\Study\Y.Mei.2013\Usermanual\CPM\Demo_data\';
-load([path 'Demo_data'])
+addpath('XXX');
+inpth='XXX';
+load([inpth 'Demo_data'])
 
-%% Baseflow separation
+%% Recession analysis and baseflow separation
 % Convert the units to mm/h
 Q=3.6*Q/A;
 Q(Q==0)=NaN;
 
-% Clustering of flow
-q=Q(~isnan(Q));
-idx=kmeans(q, 3);
-mq=nan(3, 1);
-for c=1:3
-    mq(c)=max(q(idx==c));
-end
-ben=min(mq,[],1)';
+n=1;
+TCout='XXX.mat';
 
-% RCK Method
-[Qbf_sl, pts, K, BFIm]=RCK(Q, 11, 5e-5, .9, ben, 1e-10);
-
-% FRCK Method
-BFIM=[BFIm;0;0;0;0;0;0]; % Check whether it converge
+BFI=nan(6,1);
 for y=1:6
-    [Qbf, BFIm]=FRCK(Q, Qbf_sl, K);
+% Find the rise and recession points  
+  if y==1
+    [pt, Qbf, K, ~]=RCK(Q, A, n, -11.5, .9, [], [], TCout);
+  else
+    [pt, Qbf, K, ~]=RCK(Q, A, n, -11.5, .9, [], Qbf, TCout);
+  end
 
-    BFIM(y+1)=BFIm;
+% Filter the baseflow time series 
+  [Qbf, BFIm]=FRCK(Q, Qbf, K);
+
+  BFI(y,:)=BFIm;
 end
 
 %% Event identification
 % Locate the peak flow rate from the long-term hydrograph
 LSP=.827*24*A^.2;
-Pks=CPM_peak(Q, LSP, pts.RePs, Qbf);
+Pks=CPM_peak(Q, LSP, Qbf);
 
 % Formation of flow events
-FE=CPM_FE(Pks, pts.RiPs, pts.RePs);
+FE=CPM_FE(Pks, pt.RiP, pt.ReP, Q);
 
 % Formation of rainfall episodes
 RE=CPM_RE(P, 0, 0);
@@ -50,7 +49,6 @@ for y=1:4
         RFE.V(k, :)=[];
         RFE.tE(k, :)=[];
         RFE.R(k, :)=[];
-        RFE.GEs(k, :)=[];
     end
 
     lag=RFE.D(:, 3);
@@ -60,20 +58,15 @@ for y=1:4
 end
 
 %% Extra Filters
-k=find(RFE.D(:, 3)<0 | ...
-    RFE.R(:, 1)>1 | ...
-    RFE.V(:, 2)./RFE.D(:, 2)<nanmean(Q) | ...
-    Q(RFE.tE(:, 7))./max(Q(RFE.tE(:, [2 4])), [], 2)<1/BFIm(length(BFIm)) | ...
-    min(Qbf(RFE.tE(:, [2 4]))./Q(RFE.tE(:, [2 4])), [], 2)<BFIm(length(BFIm)));
+k=find(RFE.D(:, 3)<0 | RFE.R(:, 1)>1 | RFE.V(:, 2)./RFE.D(:, 2)<nanmean(Q));
 
 RFE.D(k, :)=[];
 RFE.V(k, :)=[];
 RFE.tE(k, :)=[];
 RFE.R(k, :)=[];
 
-%% Concatenate the parameters
-para=[A K BFIm ben LSP mlag(length(mlag))];
-
 %% Save data
-clear BFIm c FE idx k K lag LSP mlag mq Pks pts q Qbf_sl RE ben y
-save([path 'Results'],'RFE','P','para','Q','Qbf');
+paTab=table(A,K,BFIm,LSP,mlag(length(mlag)),'VariableNames',{'Area','ReceCoef',...
+    'meanBFI','SearchLen','meanTlag'},'RowNames',['B' num2str(i,'%i')]);
+
+save([inpth 'Results_' num2str(i,'%i')],'RFE','paTab','Qbf');
